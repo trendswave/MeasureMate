4b4de071793193906546e6d6ee94d63a6756f34a
trendswave <paulchinedumicheal@gmail.com>
2025-01-24 06:11:39 +0100

    front: some protected route for authentiction

---
+++ client/src/components/authenticate.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+const authenticate = (req, res, next) => {
+  const token = req.headers['authorization'];
+  if (!token) {
+    return res.status(401).json({ error: 'No token provided' });
+  }
+
+  jwt.verify(token.split(' ')[1], 'your_secret_key', (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Failed to authenticate token' });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
+module.exports = authenticate;
